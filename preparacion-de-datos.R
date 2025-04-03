af3727e66c3c0c980c8fdb85a5659945005f8c83
dead-can-dance <barragan2002@gmail.com>
2025-04-02 18:36:13 -0600

    termina primera fase del analisis, falta fusionar datos

--- preparacion-de-datos.R
+++ preparacion-de-datos.R
@@ -4,3 +4,10 @@ library(readr)
 library(dplyr)
 setwd("/home/fabian/R/proyectos/analisis-bellabeat/datasets")
 read.csv("dailyActivity_merged.csv")
+
+#mutar acitividad diairia
+skim_without_charts(act_diaria)
+glimpse(act_diaria)
+CambioAct_diaria <- act_diaria %>%
+  mutate(ActivityDate = mdy(ActivityDate))
+glimpse(CambioAct_diaria)
